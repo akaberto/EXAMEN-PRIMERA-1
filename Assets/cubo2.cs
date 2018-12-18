1757cb1a6a50e2fad7b5e8d31200327dbe78f480
akaberto <43548192+akaberto@users.noreply.github.com>
2018-12-18 16:15:39 +0100

    Initial commit

---
+++ Assets/cubo2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubo2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+            OnCollisionEnter(){
+            Debug.Log Destroy 
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+    private void OnCollisionEnter()
+    {
+        throw new NotImplementedException();
+    }
+}
